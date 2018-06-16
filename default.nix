3ac59e1bdcea55a00843b16f58ecc359226299fe
Jörg Thalheim <joerg@thalheim.io>
2018-06-16 01:43:36 +0100

    add gdbgui

--- default.nix
+++ default.nix
@@ -8,6 +8,11 @@
 
   inconsolata-nerdfonts = callPackage ./inconsolata-nerdfonts {};
 
+  # patched to unlock "full" version
+  gdbgui = gdbgui.overrideAttrs (old: {
+    patches = [ ./gdbgui.patch ];
+  };
+
   #inherit (callPackages ./node-packages {})
   #  typescript-language-server; # write-good
 }
