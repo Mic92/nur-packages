578ca6b083c40767ce9698613b79bc9767313a43
Jörg Thalheim <joerg@thalheim.io>
2018-08-13 21:02:24 +0200

    cntr: 2018-06-15 -> 1.2.0

--- pkgs/cntr/default.nix
+++ pkgs/cntr/default.nix
@@ -1,13 +1,14 @@
 { stdenv, rustPlatform, fetchFromGitHub }:
 
-rustPlatform.buildRustPackage {
-  name = "cntr-2018-06-15";
+rustPlatform.buildRustPackage rec {
+  name = "cntr-${version}";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "Mic92";
     repo = "cntr";
-    rev = "967464a69ef5559a9aff2b7aa564ab53d35e4889";
-    sha256 = "13qid4qgbg9ybm5n64bbqhaj49gpsic0cvacrqsmfakvc1m1bmmg";
+    rev = version;
+    sha256 = "13qid4qgbg9ybm5n64bbqhaj49gpsic0cvacrqsmfakvc1m1bmm0";
   };
 
   cargoSha256 = "1h5d3zkiy7x0h693c6bnrwsvsc1m9wbzwpndyvyg5zlfi2gqq597";
