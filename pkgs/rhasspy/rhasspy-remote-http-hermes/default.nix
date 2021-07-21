2f52aec61667ae6f08a73506c8c8355b0d46e7cb
Jörg Thalheim <joerg@thalheim.io>
2021-07-21 18:38:17 +0200

    rhasspyPackages.rhasspy-remote-http-hermes: 0.3.2 -> 0.4.0

--- pkgs/rhasspy/rhasspy-remote-http-hermes/default.nix
+++ pkgs/rhasspy/rhasspy-remote-http-hermes/default.nix
@@ -11,19 +11,20 @@
 
 buildPythonPackage rec {
   pname = "rhasspy-remote-http-hermes";
-  version = "0.3.2";
+  version = "0.4.0";
 
   disabled = pythonOlder "3.7"; # requires python version >=3.7
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-zj5XUhGWutDlMlLv7ckz4NjqLNt6SNZmoLN2S61qCM4=";
+    sha256 = "sha256-uSK+UPv1D/n+6qwMFyYgYKlzWQG1CWO6d1lPPt9Ob6Q=";
   };
 
   postPatch = ''
     sed -i "s/aiohttp==.*/aiohttp/" requirements.txt
     sed -i "s/networkx==.*/networkx/" requirements.txt
     sed -i 's/paho-mqtt==.*/paho-mqtt/' requirements.txt
+    sed -i 's/rhasspy-nlu~=.*/rhasspy-nlu/' requirements.txt
   '';
 
   propagatedBuildInputs = [
