018b5c90ca27b915d6e98039c9f640232003f704
Jörg Thalheim <joerg@thalheim.io>
2023-04-16 16:13:57 +0200

    edge-gpt: 0.1.23 -> 0.1.24.1
    Diff: https://github.com/acheong08/EdgeGPT/compare/0.1.23...0.1.24.1

--- pkgs/edge-gpt/default.nix
+++ pkgs/edge-gpt/default.nix
@@ -1,27 +1,19 @@
 { lib
 , python3
 , fetchFromGitHub
-, fetchpatch
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "edge-gpt";
-  version = "0.1.23";
+  version = "0.1.24.1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "acheong08";
     repo = "EdgeGPT";
     rev = version;
-    hash = "sha256-MdVV7S5T8Alo9u0+k5PV22M98EM09rBcJKsBioc/QAI=";
+    hash = "sha256-oDUeNGrcEzofNNULJnnRrDZ1HP7EnoE4DUqzv9WE5yM=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/acheong08/EdgeGPT/commit/3fd920b878f9a09419aa4805c2a472bd48141116.patch";
-      sha256 = "sha256-LtgfA9Z739kV5lbpvH0Xo8iMSZ94g21AuyN9Yea2/0M=";
-    })
-  ];
 
   propagatedBuildInputs = with python3.pkgs; [
     requests
