e59ae72a1d0c8ab02ec6072689e677dcf200b7be
Jörg Thalheim <joerg@thalheim.io>
2023-06-01 21:01:52 +0200

    edge-gpt: 0.6.10 -> 0.7.1

--- pkgs/edge-gpt/default.nix
+++ pkgs/edge-gpt/default.nix
@@ -6,26 +6,32 @@
 
 python3.pkgs.buildPythonPackage rec {
   pname = "edge-gpt";
-  version = "0.6.10";
+  version = "0.7.1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "acheong08";
     repo = "EdgeGPT";
     rev = version;
-    hash = "sha256-sZ3dXy5wXojcdaR/zWMWC59JDVJT6ODo+TIL5kHyk5k=";
+    hash = "sha256-W8I9VwABXRgqAoF4y6svV0gLxuP1yN9GoASyOTbocrM=";
   };
+
+  postPatch = ''
+    # we don't need the socks feature
+    sed -i -e 's/httpx.*>=[^"]+/httpx/g' setup.py
+  '';
 
   propagatedBuildInputs = with python3.pkgs; [
     requests
+    aiofiles
     certifi
     httpx
     rich
     websockets
-    regex
     prompt-toolkit
     bing-image-creator
   ];
+
 
   pythonImportsCheck = [
     "EdgeGPT"
